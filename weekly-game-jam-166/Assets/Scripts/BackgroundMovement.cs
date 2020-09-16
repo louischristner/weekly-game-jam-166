6c8d7238b0f43be6d599b99ef78e625e59a1b6ec
louischristner <l.christner@orange.fr>
2020-09-16 20:54:11 +0200

    Create moving background

---
+++ weekly-game-jam-166/Assets/Scripts/BackgroundMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    public float speed = 1f;
+
+    public Vector2 originalPosition;
+
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector3(speed * Time.fixedDeltaTime, 0, 0));
+    }
+
+    void OnBecameInvisible()
+    {
+        transform.position = new Vector3(originalPosition.x, originalPosition.y, 0);
+    }
+}
